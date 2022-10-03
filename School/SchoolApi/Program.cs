de1167a081bcf6839f47c9822e34182e40a250e8
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-10-03 18:08:38 +0800

    Add dapper context in DI

--- School/SchoolApi/Program.cs
+++ School/SchoolApi/Program.cs
@@ -1,3 +1,5 @@
+using SchoolApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Trasient -> create new instance of DapperContext everytime.
+builder.Services.AddTransient<DapperContext>();
 
 var app = builder.Build();
 
