9191112f2668ac8fa0d6ec0ba01517226d89d7ac
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-10-03 17:51:17 +0800

    Import database in project

---
+++ School/SchoolDb/dbo/Tables/School.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[School] (
+    [Id]             INT             IDENTITY (1, 1) NOT NULL,
+    [Name]           NVARCHAR (50)   NOT NULL,
+    [Address]        NVARCHAR (50)   NOT NULL,
+    [Motto]          NVARCHAR (50)   NOT NULL,
+    [AverageTuition] DECIMAL (18, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ School/SchoolDb/dbo/Tables/Student.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Name]     VARCHAR (255) NULL,
+    [Email]    VARCHAR (255) NULL,
+    [SchoolId] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_StudentSchool] FOREIGN KEY ([SchoolId]) REFERENCES [dbo].[School] ([Id])
+);
+
