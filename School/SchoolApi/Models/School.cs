ecfd11b4d85571d58ec1ea9656629b57d5011db6
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-10-03 16:08:58 +0800

    Make model nullable

--- School/SchoolApi/Models/School.cs
+++ School/SchoolApi/Models/School.cs
@@ -3,9 +3,9 @@
     public class School
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Motto { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Motto { get; set; }
         public decimal AverageTuition { get; set; }
     }
 }
