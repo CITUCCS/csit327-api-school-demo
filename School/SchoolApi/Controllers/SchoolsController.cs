75eb5f9dfd8fd2f507c9489296032a2dd0b9b3f7
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-10-03 15:57:45 +0800

    Add SchoolsController

---
+++ School/SchoolApi/Controllers/SchoolsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetSchool(int id)
+        {
+            return Ok("Hello " + id);
+        }
+    }
+}
